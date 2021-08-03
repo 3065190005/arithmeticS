31516fe5f3d89fbbe093ed9ed8be7609074cc319
DevilFish <3065190005@qq.com>
2021-08-03 15:59:58 +0800

    init profile

---
+++ Code/adventureS.cpp
@@ -0,0 +1,18 @@
+// adventureS.cpp : 定义控制台应用程序的入口点。
+//
+
+#include <iostream>
+#include <thread>
+
+#ifdef WIN32
+#include <WinSock2.h>
+#else
+#include <socket.h>
+#endif
+
+int main()
+{
+
+    return 0;
+}
+
